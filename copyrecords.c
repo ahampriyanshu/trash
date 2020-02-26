2704b9741393c1af6c91157eceda9ba10dfdad1d
priyanshumay <tiwarimay2002@gmail.com>
2020-02-26 15:54:43 +0530

    inlcuded command line arguments

--- copyrecords.c
+++ copyrecords.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+
+string encrypt(string text, int s) 
+{ 
+    string result = ""; 
+  
+    // traverse text 
+    for (int i=0;i<text.length();i++) 
+    { 
+        // apply transformation to each character 
+        // Encrypt Uppercase letters 
+        if (isupper(text[i])) 
+            result += char(int(text[i]+s-65)%26 +65); 
+  
+    // Encrypt Lowercase letters 
+    else
+        result += char(int(text[i]+s-97)%26 +97); 
+    } 
+  
+    // Return the resulting string 
+    return result; 
+} 
+
 int main()
 {
     char ch;
--- ex.c
+++ ex.c
@@ -1,29 +1,59 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <ctype.h>
+
+
 int main( int argc, const char** argv)
 {
+	int stdin_mode,stdout_mode,com_file,reversify;
+
     char ch;
+    char *inputfile,*outputfile,*comparefile;
+    char shift;
+
+    char buffer[1500]; 
     /*Char array to store string */
     char str[24];
 
     /* Pointers for both binary files*/
     FILE *fpbr, *fpbw;
 
+     if (argc == 1)  // create pin
+    {
+    	stdin_mode=1;
+    	stdout_mode=1;
+    	com_file=0;
+    	reversify=0;
+      }
+
+  if (stdin_mode=0)
+  {
+  	for (int i = 0; i < argc; i++)
+  	{
+  		//if ( argv[i] == "-F")
+  			printf("%s\n",argv[i] );
+  			//inputfile = argv[i+1];
+  }
+
+    printf("hello are you there");
+    
     /* Open for bin1.exe file in rb mode */
-    fpbr = fopen("myfile.txt", "rb");
+    fpbr = fopen(inputfile, "rb");
 
     /* test logic for successful open*/
     if (fpbr == NULL)
     {
-        puts("Input Binary file is having issues while opening going to stdin mode");
+        puts("Input Binary file is having issues while opening terminating the program !");
       
     }
 
-     while(1)
-    {
-       if(fgets(str, 24, fpbr) ==NULL)
-            break;
-    }
+    int count = fread(&buffer, sizeof(char),1500, fpbr);
+	
 
+	// printf("Data read from file: %s \n", buffer);
+ 	// printf("Elements read: %d", count);
+   // shift = decode ();
+  //  printf("Cyper shift is %d",shift);
 
     /* Opening file bin2.exe in “wb” mode for writing*/
     fpbw= fopen("decodedfile.txt", "wb");
@@ -33,12 +63,14 @@ int main( int argc, const char** argv)
     {
        puts("Output binary file is having issues while opening going to stdout mode");
     }
-
-             fputs(str, fpbw);
-
+    else{
+             // decode(str, shift);
+             fputs(buffer, fpbw);
+         }
      /* Closing both the binary files */
      fclose(fpbr);
      fclose(fpbw);
 
      return 0;
  }
+}
