23271370ffa06a1a5dbfb00ac2cc4127fa0bb96a
priyanshumay <tiwarimay2002@gmail.com>
2020-02-27 08:32:23 +0530

    descpyt commit

--- copyrecords.c
+++ copyrecords.c
@@ -3,10 +3,32 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+string decrypt(string text,int s)
+{
+	string result="";
+	//traverse text
+	for(int i=0;i<text.length();i++)
+	{
+		//apply transformation to each character
+		//Encrypt Uppercase letters
+		if(isalpha(text[i]))
+		{
+		    if(isupper(text[i]))
+			result+=char(int(text[i]+s-65)%26 +65);
+	//Encrypt Lowercase letters
+	else
+		result+=char(int(text[i]+s-97)%26 +97);
+	}
+	else
+	{result += text[i];}
+	}
+	//Return the resulting string
+	return result;
+}
 
 int main ( int argc, char *argv[] )
 {
-	int stdin_mode,stdout_mode,com_file,reversify;
+	int read_mode,write_mode,shift_type,reversify;
 
     char ch;
     char shift;
@@ -16,60 +38,92 @@ int main ( int argc, char *argv[] )
     char str[24];
 
     /* Pointers for both binary files*/
-    FILE *fpbr, *fpbw;
+    FILE *fpbr, *fpdr,*fpbw;
 
   if (argc == 1)  // create pin
     {
-    	stdin_mode=1;
-    	stdout_mode=1;
-    	com_file=0;
+    	read_mode=0;
+    	write_mode=0;
+    	shift_type=0;
     	reversify=0;
       }
 
     // Reading file in binary mode
-  
      for (int i=1; i< argc; i++) 
     {
       if (0 == strcmp(argv[i], "-F"))
-       {
 
-       fpbr = fopen(argv[i+1], "rb");
-    if (fpbr == NULL)
+      { fpbr= fopen(argv[i+1], "rb");
+    
+          if (fpbr == NULL)
     {
-        puts("Input Binary file is having issues while opening terminating the program !");
-        exit(1);
-      
+       puts("Input binary file is having issues while opening terminating the program !");
+       exit(1);
     }
-    int count = fread( &buffer, sizeof(char),1500,fpbr) ;
-      break;
+     read_mode = 1;
+     reversify = 0;
+    }
+    }
 
-    } else {
-      
-     gets(buffer);
-}	
+    if (read_mode == 1)
+    {
+      int count = fread(&buffer, sizeof(char),500,fpbr) ; 
+    }else{
+     scanf("%s",buffer);
+    }	
+    
+         for (int i=1; i< argc; i++) 
+    {
+      if (0 == strcmp(argv[i], "-D"))
+
+      { fpdr= fopen(argv[i+1], "rb");
+    
+          if (fpdr == NULL)
+    {
+       puts("Companion binary file is having issues while opening terminating the program !");
+       exit(1);
+    }
+     shift_type=1;
+    }
+    }
+
+    if (shift_type == 1)
+    {
+      int count_com = fread(&buffer_2, sizeof(char),500,fpdr) ; 
+            if (fpdr == NULL)
+    {
+       puts("Companion binary file is having issues while opening terminating the program !");
+       exit(1);
+    }
+    decrypt(text, 26-s);
+    }
+
+
+    if (reversify ==1)
     
     /*now producing the output  file */
 
     for (int i=1; i< argc; i++) 
     {
       if (0 == strcmp(argv[i], "-O"))
-       {
 
-      fpbw= fopen(argv[i+1], "wb");
-      fputs(buffer, fpbw);
+      { fpbw= fopen(argv[i+1], "wb");
+    
           if (fpbw == NULL)
     {
        puts("Output binary file is having issues while opening terminating the program !");
        exit(1);
     }
-
-      break;
-
-    } else {
-      
-     puts(buffer);
-}	
+     write_mode=1;
     }
+    }
+
+    if (write_mode == 1)
+    {
+      int cnt = fwrite(&buffer, sizeof(char),500,fpbw) ; 
+    }else{
+     puts(buffer);
+    }	
     /* Opening file bin2.exe in “wb” mode for writing*/
    
 
