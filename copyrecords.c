74db1d7dc6150fb55d32818d331642a286925a0d
priyanshumay <tiwarimay2002@gmail.com>
2020-02-27 15:21:31 +0530

    a better tommorrow

--- copyrecords.c
+++ copyrecords.c
@@ -2,52 +2,24 @@
 #include <string.h> 
 #include <ctype.h>
 #include <stdlib.h>
-
-string decrypt(string text,int s)
-{
-	string result="";
-	//traverse text
-	for(int i=0;i<text.length();i++)
-	{
-		//apply transformation to each character
-		//Encrypt Uppercase letters
-		if(isalpha(text[i]))
-		{
-		    if(isupper(text[i]))
-			result+=char(int(text[i]+s-65)%26 +65);
-	//Encrypt Lowercase letters
-	else
-		result+=char(int(text[i]+s-97)%26 +97);
-	}
-	else
-	{result += text[i];}
-	}
-	//Return the resulting string
-	return result;
-}
-
 int main ( int argc, char *argv[] )
 {
 	int read_mode,write_mode,shift_type,reversify;
 
-    char ch;
-    char shift;
-
-    char buffer[1500]; 
-    /*Char array to store string */
-    char str[24];
-
-    /* Pointers for both binary files*/
+    char key,ch,shift;
+    int shiftkey,buffer_size,buffer_com_size;
+ 
+    /* Pointers for binary files*/
     FILE *fpbr, *fpdr,*fpbw;
 
-  if (argc == 1)  // create pin
+  if (argc == 1)  // No argument is given through command line
     {
     	read_mode=0;
     	write_mode=0;
     	shift_type=0;
     	reversify=0;
       }
-
+/**********************************************************************************************************/
     // Reading file in binary mode
      for (int i=1; i< argc; i++) 
     {
@@ -61,17 +33,23 @@ int main ( int argc, char *argv[] )
        exit(1);
     }
      read_mode = 1;
-     reversify = 0;
     }
     }
-
+    
+  if( read_mode == 1 ){
+   while ((ch = fgetc(fpbr)) != EOF) {
+ buffer_size++;
+  
+} printf("%d = %c\n",buffer_size,ch);
+char buffer[buffer_size],buffer_3[buffer_size];
     if (read_mode == 1)
     {
-      int count = fread(&buffer, sizeof(char),500,fpbr) ; 
+      int count = fread(&buffer, sizeof(char),buffer_size,fpbr) ; 
     }else{
      scanf("%s",buffer);
     }	
     
+/**********************************************************************************************************/
          for (int i=1; i< argc; i++) 
     {
       if (0 == strcmp(argv[i], "-D"))
@@ -86,24 +64,56 @@ int main ( int argc, char *argv[] )
      shift_type=1;
     }
     }
-
+  while ((ch = fgetc(fpdr)) != EOF) {
+ buffer_com_size++;
+}
+char buffer_2[buffer_com_size]; 
     if (shift_type == 1)
     {
-      int count_com = fread(&buffer_2, sizeof(char),500,fpdr) ; 
+      int count_com = fread(&buffer_2, sizeof(char),buffer_com_size,fpdr) ; 
             if (fpdr == NULL)
     {
        puts("Companion binary file is having issues while opening terminating the program !");
        exit(1);
     }
-    decrypt(text, 26-s);
+
+/**********************************************************************************************************/
+
+
+    for(int key=0; key<=25;key++){
+    char  message[buffer_com_size];
+      strcpy(message,buffer_2); 
+	for( int i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		  
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch =((ch+key-97)%26 +97);
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){ 
+			ch = ((ch+key-65)%26 +65);	
+			message[i] = ch;
+		}
+	printf ("Decoded text using shift %d is %s\n",key,message);}
+	} 
+
+printf("Choose the correct Caeser Cipher Shift");
+scanf("%d\n",&key);
+    }else{
+    key= 0;}
+    
+ 
+    /**********************************************************************************************************/
+    for (int i=1; i< argc; i++) 
+    {
+      if (0 == strcmp(argv[i], "-r"))
+      reversify=1;
+    break;
     }
 
-
-    if (reversify ==1)
-    
-    /*now producing the output  file */
-
-    for (int i=1; i< argc; i++) 
+    /**********************************************************************************************************/
+     for (int i=1; i< argc; i++) 
     {
       if (0 == strcmp(argv[i], "-O"))
 
@@ -120,19 +130,18 @@ int main ( int argc, char *argv[] )
 
     if (write_mode == 1)
     {
-      int cnt = fwrite(&buffer, sizeof(char),500,fpbw) ; 
+      //int cnt = fwrite(&buffer, sizeof(char),buffersize,fpbw) ; 
+	    fputs(buffer_3,fpbw);
     }else{
-     puts(buffer);
-    }	
-    /* Opening file bin2.exe in “wb” mode for writing*/
-   
+     puts(buffer_3);
+    }	 
 
-    /* Ensure bin2.exe opened successfully*/
-
-      
+    /**********************************************************************************************************/
      /* Closing both the binary files */
      fclose(fpbr);
      fclose(fpbw);
+     fclose(fpdr);
 
      return 0;
-}
+}}
+    
