cfaaeb46a385f2cb97cb3ff33bc2543b2f1f3b0c
priyanshumay <tiwarimay2002@gmail.com>
2020-03-28 01:41:09 +0530

    mil

--- mil.c
+++ mil.c
@@ -3,4 +3,13 @@ int main()
 {
     int angle;
     float height, distance, velocity;
+    printf("Enter height");
+    scanf("%.2f", &height);
+    printf("Enter distance");
+    scanf("%.2f", &distance);
+    printf("Enter velocity");
+    scanf("%.2f", &velocity);
+    printf("Enter angle");
+    scanf("%d", &angle);
+
 }
