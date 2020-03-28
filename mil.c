207ab05d40c79ebde68e00b052adce641c87cf16
Priyanshu Tiwari <54521023+PriyanshuMay@users.noreply.github.com>
2020-03-28 12:07:38 +0530

    Update mil.c

--- mil.c
+++ mil.c
@@ -4,16 +4,17 @@
 #define g 9.8       // defining  gravitational acceleration in m/s^2
 int main()
 {
-    int angle,set = 0;
-    float height, distance, x, formula1, formula2,velocity, range, vel,actual_range;
+    int set = 0;
+    float angle,distance,height,x, formula1, formula2,velocity, range, vel,actual_range;
     /* Declaring angle , height , velocity , radian , distance , range   */
 
     for (angle = 25; angle <= 60; angle += 2)
     {
         for (height = 2.00; height <= 4.00; height += 0.25)
-        // Converting mts to kms
+ 
         {
-            for (distance = 1.00; distance <= 3.00; distance += 00.10)
+            for (distance = 1000; distance <= 3000; distance += 100)
+                   // Converting  to kms to mts
             {
                 for (velocity = 1500; velocity <= 2000; velocity = velocity*1.10)
                 {   
@@ -28,11 +29,11 @@ int main()
                     // Progressing towards the main formula
                     range = sin(2 * x) * formula2 * (vel * vel / 2 * g);
                     // Finally the real formula
-                    
+                    printf("\n%f  %f  %f",formula1,formula2,range);
                     if (range >= 20263 + distance && range <= 20293 + distance)
                     // Checking if the mortar has reached its target successfully
                     {
-                        printf("\nSet %d is : angle = %d , distance = %f , height = %f ,  vel = %f", set, angle, distance, height,vel);
+                        printf("\nSet %d is : angle = %f , distance = %f , height = %f ,  vel = %f", set, angle, distance, height,vel);
                         set++;
                         // Displaying vlaues of all the possible set
                     }
@@ -44,6 +45,6 @@ int main()
 
     if (set == 0) //If solution isn't found
     {
-        printf("No solution. Please change the tank");
+        printf("\nNo solution. Please change the tank");
     }
 }
