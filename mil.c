1da52844ec18e933c6ae32544172c31dc5f55560
Priyanshu Tiwari <54521023+PriyanshuMay@users.noreply.github.com>
2020-03-28 11:54:21 +0530

    Update mil.c

--- mil.c
+++ mil.c
@@ -4,37 +4,39 @@
 #define g 9.8       // defining  gravitational acceleration in m/s^2
 int main()
 {
-    const float camp_dist = 20.278;
-    int angle, set = 0;
-    float height, distance, velocity, x, formula1, formula2, range, actual_range;
+    int angle,set = 0;
+    float height, distance, x, formula1, formula2,velocity, range, vel,actual_range;
     /* Declaring angle , height , velocity , radian , distance , range   */
 
-    for (angle = 25; angle < 60; angle + 2)
+    for (angle = 25; angle <= 60; angle += 2)
     {
-        for (height = 2; height <= 4; height + 0.25)
+        for (height = 2.00; height <= 4.00; height += 0.25)
+        // Converting mts to kms
         {
-            for (distance = 1; distance <= 3; distance + 0.1)
+            for (distance = 1.00; distance <= 3.00; distance += 00.10)
             {
-                for (velocity = 1500; velocity < 2000; velocity + 1.10)
-                {
+                for (velocity = 1500; velocity <= 2000; velocity = velocity*1.10)
+                {   
                     x = angle * rad;
                     // converting degree to radian
-                    formula1 = (2 * g * height) / (velocity * velocity * sin(x) * sin(x));
+                    vel = velocity * 5/18;
+                    // converting mks to si 
+                     formula1 = (2 * g * height) / (vel * vel * sin(x) * sin(x));
                     /* calculating a smaller part of the formula used for finding range of a projctile
                     launched from a height at some angle */
                     formula2 = 1 + sqrt(1 + formula1);
                     // Progressing towards the main formula
-                    range = sin(2 * x) * formula2 * (velocity * velocity / 2 * g);
+                    range = sin(2 * x) * formula2 * (vel * vel / 2 * g);
                     // Finally the real formula
-                    actual_range = camp_dist + distance;
-                    // Calculating actual range
-                    if (range >= actual_range - 15 && range <= actual_range + 15)
+                    
+                    if (range >= 20263 + distance && range <= 20293 + distance)
                     // Checking if the mortar has reached its target successfully
                     {
-                        printf("\nSet %d is : angle = %d , distance = %f , height = %f , velocity = %f", set, angle, distance, height, velocity);
+                        printf("\nSet %d is : angle = %d , distance = %f , height = %f ,  vel = %f", set, angle, distance, height,vel);
                         set++;
                         // Displaying vlaues of all the possible set
                     }
+                    
                 }
             }
         }
