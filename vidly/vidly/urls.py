423e78682f09b790fbdd6582c74bbb7ce9a0a7c7
priyanshumay <tiwarimay2002@gmail.com>
2020-06-22 06:12:31 +0530

    All done

--- vidly/vidly/urls.py
+++ vidly/vidly/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('movies/',include('movies.urls')),
     path('api/', include(movie_resource.urls))
