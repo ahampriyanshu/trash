5869b56c17a1bf7adca0958398bc703392347cd3
priyanshumay <tiwarimay2002@gmail.com>
2020-06-23 19:43:01 +0530

    removing unnecessary stuff

--- vidly/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from tastypie.resources import ModelResource
-from movies.models import Movie
-
-class MovieResource(ModelResource):
-    class Meta:
-        queryset = Movie.objects.all()
-        resource_name = 'movies'
-        excludes = ['date_created']
--- vidly/movies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name  = 'movies'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:movie_id>', views.detail, name='detail')
-] 
